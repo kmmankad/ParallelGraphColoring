62761d8b6a6272c488254cd8a6f7b0eff58373ba
Kartik Mankad <kmmankad@gmail.com>
2017-01-11 12:22:19 -0500

    Adding additional datasets from uflorida sparse matrix sets

--- bin/RunTest.pl
+++ bin/RunTest.pl
@@ -3,23 +3,45 @@
 use strict;
 use Getopt::Long;
 use Cwd;
-use Cwd 'abs_path';
+use File::Spec;
 use warnings FATAL => qw(all);
 
 my %Benchmarks = (
 	# Name => ["extension", "URL"]
-	"thermal2" => ["mtx", "http://www.cise.ufl.edu/research/sparse/MM/Schmid/thermal2.tar.gz"]
+## From UFlorida Sparse Matrix collection
+	"afshell_3" => ["mtx", "https://www.cise.ufl.edu/research/sparse/MM/Schenk_AFE/af_shell3.tar.gz"],
+	"parabolic_fem" => ["mtx", "http://www.cise.ufl.edu/research/sparse/MM/Wissgott/parabolic_fem.tar.gz"],
+	"apache2" => ["mtx", "http://www.cise.ufl.edu/research/sparse/MM/GHS_psdef/apache2.tar.gz"],
+	"ecology2" => ["mtx", "http://www.cise.ufl.edu/research/sparse/MM/McRae/ecology2.tar.gz"],
+	"thermal2" => ["mtx", "http://www.cise.ufl.edu/research/sparse/MM/Schmid/thermal2.tar.gz"],
+	"G3_circuit" => ["mtx", "http://www.cise.ufl.edu/research/sparse/MM/AMD/G3_circuit.tar.gz"],
+	"FEM_3D_thermal2" => ["mtx", "http://www.cise.ufl.edu/research/sparse/MM/Botonakis/FEM_3D_thermal2.tar.gz"],
+	"thermomech_dK" => ["mtx", "http://www.cise.ufl.edu/research/sparse/MM/Botonakis/thermomech_dK.tar.gz"],
+	"ASIC_320ks" => ["mtx", "http://www.cise.ufl.edu/research/sparse/MM/Sandia/ASIC_320ks.tar.gz"],
+	"cage13" => ["mtx", "http://www.cise.ufl.edu/research/sparse/MM/vanHeukelum/cage13.tar.gz"],
+	"atmosmodd" => ["mtx", "http://www.cise.ufl.edu/research/sparse/MM/Bourchtein/atmosmodd.tar.gz"]
+## From DIMACS
+	### TODO
 );
 
 my $binpath = "";
 my $testpath = "";
+my $onlyrun = "";
+# TODO: Add profiler stat collection support
 my $prof = 0;
 
 # Could use pod2usage...
 sub PrintUsage {
-	print "Pass path to binary with --bin=<path>\n";
+	print "Usage: \n";
+	print "Pass path to binary with --binpath=<path>\n";
+	print "Pass test-run directory (will be created) with --testdir=<folder-path>\n";
+	print "Pass the --only <name> to run only a specific benchmark by name\n";
 }
 
+###################################
+# Subroutine to run a command and
+# save the output to a log file
+#
 sub RunCmd {
 	my ($cmd, $log_file) = @_;
 	print "## RUNNING $cmd\n";
@@ -46,44 +68,99 @@ sub RunCmd {
 	return $retcode;
 }
 
-sub DownloadBenchmarks(){
-	foreach my $name (keys %Benchmarks){
-		print "## Downloading $name...\n";
-		RunCmd("wget $Benchmarks{$name}[1] -O $name.tar.gz","");
+###########################################
+# Subrotuine to download a named benchmark
+#
+sub DownloadBenchmark {
+	my $BenchmarkName = shift @_;
+	if (exists ($Benchmarks{$BenchmarkName})){
+		print "## Downloading $BenchmarkName...\n";
+		RunCmd("wget $Benchmarks{$BenchmarkName}[1] -O $BenchmarkName.tar.gz","");
 		# Unzip the guy
-		RunCmd("tar -zxvf $name.tar.gz","");
-		# Move the graph files out
-		RunCmd("mv $name/$name\.$Benchmarks{$name}[0] ./","");
+		RunCmd("tar -zxvf $BenchmarkName.tar.gz","");
+		# Move the graph files out (by appropriate extension)
+		RunCmd("mv $BenchmarkName/$BenchmarkName\.$Benchmarks{$BenchmarkName}[0] ./","");
 		# Delete the .tar.gz
-		RunCmd("rm -rf $name/ $name.tar.gz","");
+		RunCmd("rm -rf $BenchmarkName/ $BenchmarkName.tar.gz","");
+	} else {
+		die("Benchmark name $BenchmarkName not found! ABORTING. \n");
 	}
 }
 
+##########################################
+# Subroutine to download all benchmarks
+#
+sub DownloadBenchmarks(){
+	my $NumBenchmarks = scalar(keys(%Benchmarks));
+	print "## Running ALL $NumBenchmarks Benchmarks!\n";
+	foreach my $name (keys %Benchmarks){
+		DownloadBenchmark($name);
+	}
+}
+
+###########################################
+# Subroutine to run a named benchmark
+#
+sub RunBenchmark {
+	my $BenchmarkName = shift @_;
+	my $fullbinpath = "";
+	if ($binpath eq ""){
+		die("No --binpath passed! ABORTING. \n");
+	} else {
+		$fullbinpath = File::Spec->rel2abs($binpath,"..");
+		if ( -X $fullbinpath){
+			RunCmd("$fullbinpath $BenchmarkName\.$Benchmarks{$BenchmarkName}[0] $BenchmarkName\_output.txt", "$BenchmarkName\_run.log");
+		} else {
+			die("$fullbinpath is not executable! ABORTING. \n");
+		}
+	}
+}
+
+###########################################
+# Subroutine to run all benchmarks
+#
 sub RunBenchmarks(){
 	foreach my $name (keys %Benchmarks){
-		my $fullbinpath = abs_path($binpath);
-		RunCmd("$fullbinpath $name\.$Benchmarks{$name}[0] $name\_output.txt", "$name\_run.log");
+		RunBenchmark($name);
 	}
 }
 
+###########################################
+# Subroutine to create the test dir
+#
 sub SetupTestdir(){
-	print "## Creating $testpath\n";
-	RunCmd("mkdir -p $testpath","");
-	chdir $testpath;
-	print cwd;
+	if ($testpath ne ""){
+		print "## Creating $testpath\n";
+		RunCmd("mkdir -p $testpath","");
+		chdir $testpath;
+		print cwd;
+	} else {
+		die ("No --testdir passed! ABORTING. \n");
+	}
 }
 
+###########################################
+# Subroutine to print the welcome banner
+#
 sub PrintBanner(){
-	print "RunTest.pl - a simple perl script to run graph benchmarks\n";
+	print "/---------------------------------------------------------\\\n";
+	print "|RunTest.pl - a simple perl script to run graph benchmarks|\n";
+	print "\\---------------------------------------------------------/\n";
 }
 
 GetOptions('help|h' => sub { PrintUsage() and exit 0},
 	'binpath=s' => \$binpath,
 	'testdir=s' => \$testpath,
+	'only=s' => \$onlyrun,
 	'prof' => sub {$prof = 1}
 );
 
 PrintBanner();
 SetupTestdir();
-DownloadBenchmarks();
-RunBenchmarks();
+if ($onlyrun ne ""){
+	DownloadBenchmark($onlyrun);
+	RunBenchmark($onlyrun);
+}else{
+	DownloadBenchmarks();
+	RunBenchmarks();
+}
